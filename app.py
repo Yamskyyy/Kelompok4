8f167373fddf7b0a96d26f146b86011cea177127
YosafatChristofel <yosafat.nainggolan108@gmail.com>
2024-06-19 20:10:14 +0700

    Commit

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def home():
             if user_info:
                 return render_template('home.html', user_info=user_info)
             elif user_info2:
-                return render_template('home2.html', user_info2=user_info2)
+                return render_template('home2.html', user_info=user_info2)
             else:
                 return render_template('login.html')
         except jwt.ExpiredSignatureError:
@@ -116,7 +116,7 @@ def login():
                 if user_info:
                     return render_template('about.html', user_info=user_info)
                 elif user_info2:
-                    return render_template('contact.html', user_info2=user_info2)
+                    return render_template('contact.html', user_info=user_info2)
                 else:
                     return render_template('login.html')
                     
@@ -262,54 +262,104 @@ def act_week_get():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
-
-@app.route('/about2')
-def about2():
-    return render_template('about2.html')
+    token_receive = request.cookies.get(TOKEN_KEY)
+    if token_receive:
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+            user_info = db.normal_users.find_one({'username': payload.get('id')})
+            user_info2 = db.expert_users.find_one({'username': payload.get('id')})
+            
+            if user_info:
+                return render_template('about.html', user_info=user_info)
+            elif user_info2:
+                return render_template('about2.html', user_info=user_info2)
+            else:
+                return render_template('login.html')
+        except jwt.ExpiredSignatureError:
+            msg = 'Your token has expired'
+            return redirect(url_for('login', msg=msg))
+        except jwt.exceptions.DecodeError:
+            msg = 'There was a problem logging you in'
+            return redirect(url_for('login', msg=msg))
+    else:
+        return render_template('login.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    token_receive = request.cookies.get(TOKEN_KEY)
+    if token_receive:
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+            user_info = db.normal_users.find_one({'username': payload.get('id')})
+            user_info2 = db.expert_users.find_one({'username': payload.get('id')})
+            
+            if user_info:
+                return render_template('contact.html', user_info=user_info)
+            elif user_info2:
+                return render_template('contact.html', user_info=user_info2)
+            else:
+                return render_template('login.html')
+        except jwt.ExpiredSignatureError:
+            msg = 'Your token has expired'
+            return redirect(url_for('login', msg=msg))
+        except jwt.exceptions.DecodeError:
+            msg = 'There was a problem logging you in'
+            return redirect(url_for('login', msg=msg))
+    else:
+        return render_template('login.html')
 
 @app.route('/report', methods=['GET', 'POST'])
 @admin_or_user
 def report():
-    if request.method == 'POST':
-        data_type = request.form['data_type']
-        name = request.form['name']
-        position_or_class = request.form['position_or_class']
-        hadir = request.form.get('hadir') == 'on'
-        sakit = request.form.get('sakit') == 'on'
-        izin = request.form.get('izin') == 'on'
-        tanpa_keterangan = request.form.get('tanpa_keterangan') == 'on'
-        
-        doc = {
-            'name': name,
-            'position_or_class': position_or_class,
-            'hadir': hadir,
-            'sakit': sakit,
-            'izin': izin,
-            'tanpa_keterangan': tanpa_keterangan,
-        }
-        
-        if data_type == 'anak':
-            db.absensi_anak.insert_one(doc)
-        elif data_type == 'staff':
-            db.absensi_staff.insert_one(doc)
-        
-        return redirect(url_for('report'))
-    
-    absensi_anak = list(db.absensi_anak.find({}, {'_id': False}))
-    absensi_staff = list(db.absensi_staff.find({}, {'_id': False}))
-    return render_template('report.html', absensi_anak=absensi_anak, absensi_staff=absensi_staff)
+    token_receive = request.cookies.get(TOKEN_KEY)
+    if token_receive:
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+            user_info = db.normal_users.find_one({'username': payload.get('id')}) or db.expert_users.find_one({'username': payload.get('id')})
+            
+            if request.method == 'POST':
+                data_type = request.form['data_type']
+                name = request.form['name']
+                position_or_class = request.form['position_or_class']
+                hadir = request.form.get('hadir') == 'on'
+                sakit = request.form.get('sakit') == 'on'
+                izin = request.form.get('izin') == 'on'
+                tanpa_keterangan = request.form.get('tanpa_keterangan') == 'on'
+                
+                doc = {
+                    'name': name,
+                    'position_or_class': position_or_class,
+                    'hadir': hadir,
+                    'sakit': sakit,
+                    'izin': izin,
+                    'tanpa_keterangan': tanpa_keterangan,
+                }
+                
+                if data_type == 'anak':
+                    db.absensi_anak.insert_one(doc)
+                elif data_type == 'staff':
+                    db.absensi_staff.insert_one(doc)
+                
+                return redirect(url_for('report'))
+            
+            absensi_anak = list(db.absensi_anak.find({}, {'_id': False}))
+            absensi_staff = list(db.absensi_staff.find({}, {'_id': False}))
+            return render_template('report.html', absensi_anak=absensi_anak, absensi_staff=absensi_staff, user_info=user_info)
+        except jwt.ExpiredSignatureError:
+            msg = 'Your token has expired'
+            return redirect(url_for('login', msg=msg))
+        except jwt.exceptions.DecodeError:
+            msg = 'There was a problem logging you in'
+            return redirect(url_for('login', msg=msg))
+    else:
+        return render_template('login.html')
 
 @app.route('/download_report/<report_type>')
 def download_report(report_type):
+    absensi_anak = list(db.absensi_anak.find({}, {'_id': False}))
+    absensi_staff = list(db.absensi_staff.find({}, {'_id': False}))
+    
     if report_type == 'csv':
-        absensi_anak = list(db.absensi_anak.find({}, {'_id': False}))
-        absensi_staff = list(db.absensi_staff.find({}, {'_id': False}))
-        
         output = StringIO()
         writer = csv.writer(output)
         writer.writerow(['Name', 'Position/Class', 'Hadir', 'Sakit', 'Izin', 'Tanpa Keterangan'])
@@ -323,9 +373,6 @@ def download_report(report_type):
         return send_file(output, mimetype='text/csv', attachment_filename='report.csv', as_attachment=True)
     
     elif report_type == 'pdf':
-        absensi_anak = list(db.absensi_anak.find({}, {'_id': False}))
-        absensi_staff = list(db.absensi_staff.find({}, {'_id': False}))
-        
         # Generate PDF using reportlab
         buffer = BytesIO()
         c = canvas.Canvas(buffer, pagesize=letter)
